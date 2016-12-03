011de1897bbe87eb9a1996482c511e45cbce30ce
Kevin <kevin@potatofrom.space>
2016-12-03 12:51:17 -0500

    Implement file list & encrypting with static pw

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
+var path = require('path');
+var WriteFilePlugin = require('write-file-webpack-plugin');
+
 module.exports = {
     entry: './app/app.js',
+    devServer: {
+        outputPath: path.join(__dirname, './dist')
+    },
     output: {
         path: './dist',
         filename: 'app.bundle.js'
-    }
+    },
+    plugins: [
+      new WriteFilePlugin()
+    ]
 };
